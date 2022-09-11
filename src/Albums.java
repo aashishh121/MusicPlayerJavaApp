e0c3baa8841a95e131102669df57777623d6b4b6
Ashish Kumar <98329891+aashishh121@users.noreply.github.com>
2022-09-11 09:43:27 +0530

    Add files via upload

---
+++ src/Albums.java
@@ -0,0 +1,72 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Albums {
+	
+	private String name;
+	private String artist;
+	private ArrayList<Songs> songs;
+	
+	public Albums(String name, String artist) {
+		this.name = name;
+		this.artist = artist;
+		this.songs = new ArrayList<Songs>();
+	}
+	
+	public Albums() {
+		
+	}
+	
+	public Songs findSong(String title) {
+		for(Songs checkedSongs : songs) {
+			if(checkedSongs.getTitle().equals(title)) {
+				return checkedSongs ;
+			}
+		}
+		return null;
+		
+	}
+	
+	public boolean addSong(String title, double duration) {
+		if(findSong(title) == null) {
+			songs.add(new Songs(title,duration));
+			//System.out.println(title + "Successful added to the list");
+			return true;
+		}else {
+			//System.out.println("Song with name" + title + "already exists in the list");
+			return false;
+		}
+	}
+	
+	public boolean addToPlayList(int trackNumber, LinkedList<Songs> PlayList) {
+		int index = trackNumber - 1;
+		if(index > 0 && index <= this.songs.size()) {
+			PlayList.add(this.songs.get(index));
+			return true;
+		}
+		//System.out.println("this album does not have song with this trackNumber" + trackNumber) ;
+		return false;
+	}
+	
+	public boolean addToPlayList(String title, LinkedList<Songs> PlayList) {
+		for(Songs checkedSongs : this.songs) {
+			
+			if(checkedSongs.getTitle().equals(title)) {
+				PlayList.add(checkedSongs);
+				return true;
+				
+			}
+		}
+		//System.out.println(title +"there is no such songs in album");
+		return false;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ src/Songs.java
@@ -0,0 +1,33 @@
+
+public class Songs {
+	String title;
+	double duration;
+	
+	
+	public Songs(String title, double duration) {
+		this.title = title;
+		this.duration = duration;
+	}
+	
+	public Songs() {
+		
+	}
+	
+	public String getTitle() {
+		return title;
+		
+	}
+	
+	public double getDuration() {
+		return duration;
+	}
+	
+	@Override
+    public String toString() {
+        return "Song{" +
+                "title='" + title + '\'' +
+                ", duration=" + duration +
+                '}';
+    }
+	
+}
